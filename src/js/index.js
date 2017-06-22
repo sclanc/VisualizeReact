6ecf25650ee8b7396d209301c370e9f395495e58
Sean Clancy <sean@Seans-Mac-mini.fios-router.home>
2017-06-22 18:31:21 -0400

    proof of concept

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../scss/index.scss';
+import VisualizeReact from './App';
+
+const element = document.getElementById('content');
+ReactDOM.render(<VisualizeReact />, element);
+
